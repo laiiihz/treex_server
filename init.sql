1105e6ef3e39e3a5552954258d04a62732cb17c5
laiiihz <laiiihz@outlook.com>
2020-02-18 13:03:17 +0800

    update init sql

--- init.sql
+++ init.sql
@@ -1,14 +1,18 @@
 create table login_user
 (
-    id       int auto_increment
+    id              int auto_increment
         primary key,
-    name     char(30) charset utf8 not null,
-    password char(255)             not null,
-    phone    char(20)              null,
-    email    char(255)             null,
-    avatar   char(255)             null,
+    name            char(30) charset utf8 not null,
+    password        char(255)             not null,
+    phone           char(20)              null,
+    email           char(255)             null,
+    avatar          char(255)             null,
+    background      char(255)             null,
+    backgroundColor int                   null,
+    constraint login_user_email_uindex
+        unique (email),
     constraint login_user_name_uindex
-        unique (name)
+        unique (name),
+    constraint login_user_phone_uindex
+        unique (phone)
 );
-
-
